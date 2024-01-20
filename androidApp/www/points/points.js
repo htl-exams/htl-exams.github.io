d5a8e3958c6964d6fcc389424c356d3b5fd34cf4
htl-exams <htlexams@gmail.com>
2024-01-20 13:55:11 +0100

    Remove files

--- androidApp/www/points/points.js
+++
@@ -1,6 +0,0 @@
-import {addPoint} from '../js/points.js';
-
-const btn = document.getElementById('addPoint');
-
-btn.addEventListener('click', addPoint);
-        
--- points/points.js
+++
@@ -1,5 +0,0 @@
-import { addPoint } from '../js/points.js';
-
-const btn = document.getElementById('addPoint');
-
-btn.addEventListener('click', addPoint);
