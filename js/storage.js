848e4c16139bb2e1712cc1963090db4f1cb61ab4
htl-exams <154881142+htl-exams@users.noreply.github.com>
2024-01-26 13:37:23 +0100

    Commit

--- js/storage.js
+++ js/storage.js
@@ -4,14 +4,15 @@ import { getStorage, ref, uploadBytes, listAll, getDownloadURL } from "https://w
 const storage = getStorage(app);
 
 
-function upload(input) {
+function upload(input, success = null, error = null) {
     const fileItem = input.files[0];
     const fileName = fileItem.name;
 
     let storageRef = ref(storage, `images/${fileName}`);
     uploadBytes(storageRef, fileItem).then((snapshot) => {
-        console.log(snapshot);
-        console.log('Uploaded a blob or file!');
+        if (success) success();
+    }).catch((err) => {
+        if (error) error(err);
     });
 }
 
